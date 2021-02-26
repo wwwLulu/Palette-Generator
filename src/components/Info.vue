9d8baf9c34f0e1b1b4d7f4d24b6657d63975dd58
wwwLulu <amongthestars9898@gmail.com>
2021-02-25 22:11:10 -0800

    Info button more info

--- src/components/Info.vue
+++ src/components/Info.vue
@@ -1,8 +1,33 @@
 <template>
-    <i class="fas fa-info"></i>
+    <i @click="open = true" v-show="!open" class="fas fa-info"></i>
+    <div class="info" v-show="open">
+        <i @click="open = false" class="fas fa-times"></i>
+        <p>
+            Made entirely with Vue.js & Palette generated using ColorThief
+            library
+        </p>
+    </div>
 </template>
 
-<style lang="scss" scoped>
+<script>
+export default {
+    data() {
+        return {
+            open: false,
+        }
+    },
+}
+</script>
+
+<style scoped>
+.info p {
+    position: absolute;
+    bottom: 8rem;
+    right: 8rem;
+    font-size: 1.6rem;
+    color: var(--color-secondary);
+}
+
 i {
     cursor: pointer;
     position: absolute;
@@ -11,9 +36,16 @@ i {
     bottom: 2rem;
     right: 2rem;
     background: var(--color-secondary);
-    padding: 1.5rem 2.1rem;
+
     border-radius: 50%;
     box-shadow: 0 10px 15px -3px rgba(0, 0, 0, 0.1),
         0 4px 6px -2px rgba(0, 0, 0, 0.05);
+}
+
+.fa-info {
+    padding: 1.5rem 2.1rem;
+}
+.fa-times {
+    padding: 1.5rem 1.7rem;
 }
 </style>
